427b912dc3ff668fc19b4dbbbdb50faf27e2eed4
Rainer Frey <frey.rainer@gmail.com>
2017-03-21 19:05:57 +0100

    Spring Boot 1.5.2 project

---
+++ src/main/java/de/mrfrey/LinkEncodingProblemApplication.java
@@ -0,0 +1,12 @@
+package de.mrfrey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkEncodingProblemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkEncodingProblemApplication.class, args);
+	}
+}
---
+++ src/test/java/de/mrfrey/LinkEncodingProblemApplicationTests.java
@@ -0,0 +1,16 @@
+package de.mrfrey;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LinkEncodingProblemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
